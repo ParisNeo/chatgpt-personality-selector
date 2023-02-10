6fd26ff11ac36a48f555088b3a820287efc97a82
Saifeddine <aloui.seifeddine@gmail.com>
2023-02-10 22:44:00 +0100

    test

--- extra/translator_parallel_threads.py
+++ extra/translator_parallel_threads.py
@@ -2,6 +2,7 @@ import concurrent.futures
 from mbart_translator import MBartTranslator
 import pandas as pd
 from tqdm import tqdm
+from pathlib import Path
 
 # Load text
 df = pd.read_csv("prompts_en-US.csv", quotechar='"')
@@ -17,13 +18,16 @@ def translate_text(text, column, index):
     tqdm.write(f"Translating '{text}' to '{translated_text}'")
     return translated_text
 
-for column in tqdm(df_fr.columns):
-    mask = df_fr[column].notna()
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(translate_text, df_fr[column].iloc[index], column, index)
-                   for index in df_fr[column][mask].index]
 
-        for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
-            future.result()
+# for column in tqdm(df_fr.columns):
+#     mask = df_fr[column].notna()
+#     with concurrent.futures.ThreadPoolExecutor() as executor:
+#         futures = [executor.submit(translate_text, df_fr[column].iloc[index], column, index)
+#                    for index in df_fr[column][mask].index]
 
-df_fr.to_csv("prompts_fr-FR_out.csv", quotechar='"',index=False)
+#         for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
+#             future.result()
+
+folder = Path(__file__).parent
+print(f"Exporting the data to {folder}")
+df_fr.to_csv(folder/"prompts_fr-FR_out.csv", quotechar='"',index=False)
