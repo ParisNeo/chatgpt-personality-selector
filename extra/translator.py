0f53323e900387319d3ef0786e0ae5ebad8f9eca
Saifeddine <aloui.seifeddine@gmail.com>
2023-02-10 23:00:21 +0100

    upgraded

--- extra/translator.py
+++ extra/translator.py
@@ -1,22 +1,25 @@
 from mbart_translator import MBartTranslator
 import pandas as pd
 from tqdm import tqdm
+from pathlib import Path
 
 # Load text
 df = pd.read_csv("prompts_en-US.csv", quotechar='"')
 
 print(df)
-print("Loading model")
-translator = MBartTranslator()
+# print("Loading model")
+# translator = MBartTranslator()
 df_fr = df.copy()
 
-print("Translating")
-for column in tqdm(df_fr.columns):
-    mask = df_fr[column].notna()
-    for index in tqdm(df_fr[column][mask].index):
-        text = df_fr[column].iloc[index]
-        translated_text = translator.translate(text, "en_XX", "fr_XX")
-        df_fr[column].iloc[index] = translated_text
-        tqdm.write(f"Translating '{text}' to '{translated_text}'")
+# print("Translating")
+# for column in tqdm(df_fr.columns):
+#     mask = df_fr[column].notna()
+#     for index in tqdm(df_fr[column][mask].index):
+#         text = df_fr[column].iloc[index]
+#         translated_text = translator.translate(text, "en_XX", "fr_XX")
+#         df_fr[column].iloc[index] = translated_text
+#         tqdm.write(f"Translating '{text}' to '{translated_text}'")
 
-df_fr.to_csv("prompts_fr-FR_out.csv", quotechar='"',index=False)
+folder = Path(__file__).parent
+print(f"Exporting the data to {folder}")
+df_fr.to_csv(folder/"prompts_fr-FR_out.csv", quotechar='"',index=False)
