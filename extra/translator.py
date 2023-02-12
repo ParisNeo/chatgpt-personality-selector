630db9173fd2ad77594c5602a41776e9be3d5445
Saifeddine <aloui.seifeddine@gmail.com>
2023-02-13 00:29:49 +0100

    upgraded

--- extra/translator.py
+++ extra/translator.py
@@ -4,7 +4,11 @@ from tqdm import tqdm
 from pathlib import Path
 
 src = "en_XX"
-dst = "ar_AR"
+dsts = [
+    "it_IT",
+    "de_DE",
+    "es_XX"
+]
 output = "prompts_ar-AR.csv"
 
 # Load text
@@ -14,18 +18,18 @@ print(df)
 print("Loading model")
 translator = MBartTranslator()
 df_fr = df.copy()
+for dst in dsts:
+    print("Translating")
+    for column in tqdm(df_fr.columns):
+        mask = df_fr[column].notna()
+        for index in tqdm(df_fr[column][mask].index):
+            text = df_fr[column].iloc[index]
+            translated_text = translator.translate(text, src, dst)
+            df_fr[column].iloc[index] = translated_text
+            tqdm.write(f"Translating '{text}' to '{translated_text}'")
 
-print("Translating")
-for column in tqdm(df_fr.columns):
-    mask = df_fr[column].notna()
-    for index in tqdm(df_fr[column][mask].index):
-        text = df_fr[column].iloc[index]
-        translated_text = translator.translate(text, src, dst)
-        df_fr[column].iloc[index] = translated_text
-        tqdm.write(f"Translating '{text}' to '{translated_text}'")
 
-
-folder = Path(__file__).parent.parent/"languages"
-output_file= folder/output
-print(f"Exporting the data to {output_file}")
-df_fr.to_csv(output_file, quotechar='"',index=False)
+    folder = Path(__file__).parent.parent/"languages"
+    output_file= folder/output
+    print(f"Exporting the data to {output_file}")
+    df_fr.to_csv(output_file, quotechar='"',index=False)
